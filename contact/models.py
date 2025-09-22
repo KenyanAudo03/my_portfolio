c02d73ab30a39132e85f86fe8c4d3afedff1c3cb
Victor Owino <vo4685336@gmail.com>
2025-09-22 21:36:01 +0300

    add ContactInfo model for social and contact links

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactInfo(models.Model):
+    whatsapp_number = models.CharField(max_length=20, blank=True, null=True)
+    linkedin_url = models.URLField(max_length=255, blank=True, null=True)
+    x_url = models.URLField(max_length=255, blank=True, null=True)
+    github_url = models.URLField(max_length=255, blank=True, null=True)
+    telegram_url = models.URLField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    instagram_url = models.URLField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Contact Info ({self.email or 'No email'})"
