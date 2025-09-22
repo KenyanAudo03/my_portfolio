6fc63c0f7014228edf202239cb8a5510c1955bc9
Victor Owino <vo4685336@gmail.com>
2025-09-22 21:43:23 +0300

    enforce single ContactInfo instance in admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
 from .models import ContactInfo
+
+
+class SingleContactInfoForm(ModelForm):
+    def clean(self):
+        if ContactInfo.objects.exists() and not self.instance.pk:
+            raise ValidationError("Only one ContactInfo instance is allowed.")
+        return super().clean()
 
 
 @admin.register(ContactInfo)
 class ContactInfoAdmin(admin.ModelAdmin):
+    form = SingleContactInfoForm
     list_display = ("email", "whatsapp_number", "linkedin_url", "github_url")
     search_fields = ("email", "whatsapp_number")
